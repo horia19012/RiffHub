cbe717d5070bdfc2347c2d7cc5430edf07e8499c
horia19012 <apostol_dan1901@yahoo.com>
2026-02-18 17:05:47 +0200

    gitignore update

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d2121048007e41ca5e9b21ca37b7dbe3cee95c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+effed56a2e1a7a7aaeb07640087d82ac2d881579")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
